5b69439b14f4a108dd7e3a9b7f42537f6b0f27e5
Santiago Buitrago <jsbuitragorojas@gmail.com>
2023-10-12 19:17:22 -0500

    Updated

--- src/app/components/Coordinador/ver-puesto-coordinador/ver-puesto-coordinador.component.ts
+++ src/app/components/Coordinador/ver-puesto-coordinador/ver-puesto-coordinador.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../services/api/api.service';
 import { UntypedFormGroup, UntypedFormBuilder } from '@angular/forms';
+import { forkJoin, map } from 'rxjs';
 
 @Component({
   selector: 'app-ver-puesto-coordinador',
@@ -11,6 +12,7 @@ export class VerPuestoCoordinadorComponent implements OnInit {
   tabla: boolean = false;
   percent: number = 0;
   dataStations: any = [];
+  
   searchForm: UntypedFormGroup = this.fb.group({
     puestos: [null],
   });
@@ -21,36 +23,34 @@ export class VerPuestoCoordinadorComponent implements OnInit {
 
   ngOnInit() {
     this.getPuestos();
+    this.tabla = true;
   }
 
-  getSelectedStation(item: any) {
-    if (item) {
-      const codigo_unico = this.getCode(item);
-      const data = { puesto: codigo_unico };
-      this.getNecesitadosPuesto(data);
-      this.tabla = true;
-    } else {
-      this.tabla = false;
-    }
-  }
+
 
   getPuestos() {
+     
     this.apiService.getStationsTestigo().subscribe((resp: any) => {
-      this.dataStations = resp;
-      if (this.dataStations.length > 0) {
-        this.searchForm
-          .get('puestos')
-          ?.setValue(this.dataStations[0].codigo_unico);
-        this.getSelectedStation(this.dataStations[0]);
-      }
+      this.dataStations = this.dataStations.concat(resp);
+     
+      this.loadAllPuestosData();
+    });
+  }
+  
+
+
+  
+  getNecesitadosPuesto(data: any, nombre: any) {
+    this.apiService.getNecesitadosPuesto(data).subscribe((resp: any) => {
+      const state = { nombre }; 
+      this.dataStateStation = [resp];
+      console.log(state)
+     this.dataStateStation[0].nombre = state.nombre
+      console.log(this.dataStateStation)
     });
   }
 
-  getNecesitadosPuesto(data: any) {
-    this.apiService.getNecesitadosPuesto(data).subscribe((resp: any) => {
-      this.dataStateStation = [resp];
-    });
-  }
+  
 
   createPercent(existentes: any, necesitados: any) {
     const percent = Math.round((existentes / necesitados) * 100) / 100;
@@ -80,7 +80,52 @@ export class VerPuestoCoordinadorComponent implements OnInit {
     return codigo_unico;
   }
 
+  getName(item: any) {
+    const { nombre } = item.nombre;
+    return nombre;
+  }
+
   stateSeleccionado(state: any) {
     this.stateActual = state;
   }
+
+  // ...
+
+  getSelectedStation(item: any) {
+    if (item) {
+      const codigo_unico = this.getCode(item);
+      const data = { puesto: codigo_unico };
+      const nombre = item.nombre
+      this.getNecesitadosPuesto(data, nombre);
+    } else {
+      // Si no se selecciona un puesto, cargar la información de todos los puestos.
+      this.loadAllPuestosData();
+    }
+    this.tabla = true;
+  }
+  loadAllPuestosData() {
+    this.dataStateStation = []; // Asegúrate de que dataStateStation esté vacío antes de cargar los datos.
+    console.log(this.dataStations)
+    const requests = this.dataStations.map((item: any) => {
+     
+      const codigo_unico = this.getCode(item);
+      const data = { puesto: codigo_unico };
+      const nombre = item.nombre
+      return this.apiService.getNecesitadosPuesto(data).pipe(
+        map((response: any) => ({ ...response, nombre })) // Agrega 'nombre' al objeto 'response'
+      );
+  
+    });
+  
+    forkJoin(requests).subscribe((responses: any) => {
+      this.dataStateStation = responses;
+      console.log(this.dataStateStation)
+    });
+    
+   
+    
+  }
+  
+  
+
 }
